7ddd511a33e8c299d10fe53e507ec9c2be8f157f
Santript Mehta <santriptmehta@gmail.com>
2024-08-31 23:17:22 +0530

    Project Notion

---
+++ src/main/java/com/leafBlog/Leaf/Blog/LeafBlogApplication.java
@@ -0,0 +1,13 @@
+package com.leafBlog.Leaf.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leafBlog/Leaf/Blog/LeafBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leafBlog.Leaf.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
